c47211aa0f8f35933f4f7c04fc91415946c85458
Ernest Roca <ernst.roca@gmail.com>
2011-03-03 11:39:32 +0100

    Creació del Projecte

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transman::Application.config.secret_token = 'b08014eb3fecd4305b3413f357a3997949b3ad04ef2866ad3fe43f70e01161c98f0a174dc346e15a357ae659ebfcff2fc0db4c15b84a8545f890d7b1cb161585'
